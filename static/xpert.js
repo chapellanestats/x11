4697ceccb9ccd23e2e15167c3e84b515521ca85c
chapellanestats <91821183+chapellanestats@users.noreply.github.com>
2021-10-28 21:48:44 +1100

    Add files via upload

--- static/xpert.js
+++ static/xpert.js
@@ -1,25 +1,24 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-    
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.dropdown');
+  const navLinks = document.querySelectorAll('.dropdown button');
 
-    burger.addEventListener('click', () => {
-        //Toggle Nav
-        nav.classList.toggle('nav-active');
+  burger.addEventListener('click', () => {
+    //Toggle Nav
+    nav.classList.toggle('nav-active');
 
-        //Animate Links
-        navLinks.forEach((link, index) => {
-            if(link.style.animation) {
-                link.style.animation = ''
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
-            }
-        });
-        //Burger animation
-        burger.classList.toggle('toggle');
+    //Animate Links
+    navLinks.forEach((link, index) => {
+        if(link.style.animation) {
+            link.style.animation = ''
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 10 + 0.2}s`;
+        }
     });
-    
+    //Burger animation
+    burger.classList.toggle('toggle');
+});
+
 }
 
 navSlide();
